ca122b01dad12625bc3716ef4370de0d65dcb11d
Ben Baer <benbaer17@gmail.com>
2018-12-02 00:55:29 -0500

    Day 2

--- 2018/day_2/p1.py
+++ 2018/day_2/p1.py
@@ -1,1 +1,21 @@
+import collections
 
+def main():
+  sum2=0
+  sum3=0
+  
+  with open("d2_input", 'r') as file:
+    lines = file.readlines()
+  for line in lines:
+    import collections
+    letterfreq = collections.Counter(line)
+    letterinfo=sorted(dict(letterfreq).values(), reverse=True)
+    if(2 in letterinfo):
+      sum2+=1;
+    if(3 in letterinfo):
+      sum3+=1;
+
+    
+  print(sum2 * sum3)
+
+main()
--- 2018/day_2/p2.py
+++ 2018/day_2/p2.py
@@ -1,1 +1,12 @@
-
+import difflib
+def main():
+  with open("d2_input", 'r') as file:
+    lines = file.readlines()
+  lines = sorted(lines)
+  for i in range(len(lines)-1):
+    output=[li for li in difflib.ndiff(lines[i],lines[i+1]) if li[0] != ' ']
+    if(len(output) == 2):
+      print(lines[i])
+      print(lines[i+1])
+      print(output)
+main()
